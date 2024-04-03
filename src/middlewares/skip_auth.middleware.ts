4fd4e853b3b79ea815e2dddedb595d9d32fba3a5
Nandan Magdum <nandanmagdum14@gmail.com>
2024-04-03 22:33:01 +0530

    removed auth for login and signUP

--- src/middlewares/skip_auth.middleware.ts
+++ src/middlewares/skip_auth.middleware.ts
@@ -2,8 +2,11 @@ import {Request, Response, NextFunction} from "express";
 import { authenticateUser } from "./auth.middleware";
 
 export const skipAuth = async(req:Request, res:Response, next:NextFunction) => {
-    
-    if(req.path == "/api/v1/auth/register" || req.path == "/api/v1/auth/login"){
+    console.log("req path = ", req.path);
+    if(req.path == "/api/v1/auth/register" || req.path == "/api/v1/auth/login" || 
+        req.path === "/api/v1/auth/send_otp" || req.path === "/api/v1/auth/verify_otp" ||
+        req.path === "/api/v1/auth/login"
+    ){
     console.log("++++++++++++++++",req.path);
 
         next();
